3f5c012f759255ceb74a39a21c566943e764c0d5
NattananNice <nattananjk@gmail.com>
2023-05-31 16:19:40 +0700

    first

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<h1>Contact</h1>
+<ul>
+    <li>
+        <a href="/">Welcome</a>
+    </li>
+    <li>
+        <a href="/post">Post Pege</a>
+    </li>
+    <li>
+        <a href="/profile">Profile Pege</a>
+    </li>
+    <li>
+        <a href="/contact">Contact Pege</a>
+    </li>
+    <li>
+        <a href="/link">Link Pege</a>
+    </li>
+</ul>
